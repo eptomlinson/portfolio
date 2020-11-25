bfd8cc0b453ebc3818b685d592cac5db708266c0
Erik Tomlinson <eptomlinson@gmail.com>
2020-11-24 20:16:29 -0600

    added server js

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from './About';
+
+const Contact = () => {
+
+    return (
+        <div className="infoContainer">
+
+
+        </div>
+    )
+};
+
+export default Contact;
