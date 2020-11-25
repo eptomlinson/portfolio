38d26c9cf51e4f2dac5f14cab2cdd634b50091ea
Erik Tomlinson <eptomlinson@gmail.com>
2020-11-24 20:38:33 -0600

    added footer and lorem ipsums to info index

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Hero from './components/Hero';
 import InfoSection from './components/InfoSection';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Hero></Hero>
     <Navbar></Navbar>
     <InfoSection></InfoSection>
+    <Footer></Footer>
     </>
   );
 };
---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="page-footer font-small gray">
+            <div className="footer-copyright text-center">
+                <p>2020 Copyright Erik Tomlinson</p>
+            </div>
+
+        </footer>
+    )
+};
+
+export default Footer;
